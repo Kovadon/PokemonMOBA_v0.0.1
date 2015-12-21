25bc2a0519e4881b8942261809221d2c1f31851f
TijmenW <TijmenW@users.noreply.github.com>
2015-12-21 16:29:56 +0100

    use SceneManager in menu connect button

--- Assets/Scripts/MenuConnectButton.cs
+++ Assets/Scripts/MenuConnectButton.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuConnectButton : MonoBehaviour {
 
 	public void ChangeToScene (string sceneToChangeTo) {
-        Application.LoadLevel(sceneToChangeTo);
+        SceneManager.LoadScene(sceneToChangeTo);
 	}
 }
